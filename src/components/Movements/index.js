86782c777e891de41c00572c2801787d2ce67ba7
elisonmartins <martinselison10@gmail.com>
2023-05-08 20:36:42 -0300

    feat: adicionando OnPress nos items

--- src/components/Movements/index.js
+++ src/components/Movements/index.js
@@ -2,16 +2,24 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Movements({ data }) {
+  
+  const [showValue, setShowvalue] = useState(false)
+
  return (
-   <TouchableOpacity style={styles.container}>
+   <TouchableOpacity style={styles.container} onPress={ () => setShowvalue(!showValue)}>
     <Text style={styles.date}>{data.date}</Text>
 
     <View style={styles.content}>
       <Text style={styles.label}>{data.label}</Text>
 
-      <Text style={data.type === 1 ? styles.value : styles.expenses}>
+      { showValue ? (
+        <Text style={data.type === 1 ? styles.value : styles.expenses}>
         R${data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
         </Text>
+      ) : (
+        <View style={styles.skeleton}>
+        </View>
+      )}
     </View>
 
    </TouchableOpacity>
@@ -48,5 +56,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#e74c3c',
     fontWeight: 'bold',
+  },
+  skeleton: {
+    marginTop: 6,
+    width: 80,
+    height: 10,
+    backgroundColor: '#DADADA',
+    borderRadius: 8,
   },
 })
